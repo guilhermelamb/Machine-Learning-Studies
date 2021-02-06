9ae5ca4bc9ea658e5cfb1549367c02e8745a5e77
Guilherme <guilhermelamb@gmail.com>
2021-02-05 22:32:31 -0300

    Uploaded logistic regression files

---
+++ Logistic-Regression/regressao_logistica_credit_data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+import collections
+
+
+base = pd.read_csv('credit_data.csv')
+
+base.loc[base.age < 0, 'age'] = 40.92
+
+previsores = base.iloc[:, 1:4].values
+classe = base.iloc[:,4].values
+
+imputer = SimpleImputer()
+imputer = imputer.fit(previsores[:,1:4])
+previsores[:,1:4] = imputer.transform(previsores[:,1:4])
+
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+previsores_train, previsores_test, classe_train, classe_test = train_test_split(previsores,classe, test_size=0.25, random_state=0)
+
+classificador = LogisticRegression(random_state = 1)
+
+classificador.fit(previsores_train, classe_train)
+
+resultado = classificador.predict(previsores_test)
+
+accuracy = accuracy_score(classe_test,resultado)
+matriz = confusion_matrix(classe_test, resultado)
+
+collections.Counter(classe_test)
